0e82382c5c5c503b7031d5c524a5c98edeaf81f1
PannnTastic <pannntastic@gmail.com>
2025-12-01 15:37:19 +0700

    route public page

--- routes/public.js
+++ routes/public.js
@@ -3,13 +3,31 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/public/dashboardController');
+const pageController = require('../controllers/public/pageController');
 
 // Menampilkan Halaman Utama (Dashboard Publik)
-// Ini adalah endpoint pertama: GET /
 router.get('/', dashboardController.renderDashboard);
 
-// (Nantinya Anda bisa tambahkan rute lain di sini)
-// router.get('/kegiatan', ...);
-// router.get('/produk/:id', ...);
+// Kegiatan Routes
+router.get('/kegiatan', pageController.renderKegiatan);
+router.get('/kegiatan/:id', pageController.renderKegiatanDetail);
+
+// Produk Routes
+router.get('/produk', pageController.renderProduk);
+router.get('/produk/:id', pageController.renderProdukDetail);
+
+// Prestasi Routes
+router.get('/prestasi', pageController.renderPrestasi);
+router.get('/prestasi/:id', pageController.renderPrestasiDetail);
+
+// Anggota Routes
+router.get('/anggota', pageController.renderAnggota);
+router.get('/anggota/:id', pageController.renderAnggotaDetail);
+
+// Galeri Routes
+router.get('/galeri', pageController.renderGaleri);
+
+// Testimoni Routes
+router.get('/testimoni', pageController.renderTestimoni);
 
 module.exports = router;
